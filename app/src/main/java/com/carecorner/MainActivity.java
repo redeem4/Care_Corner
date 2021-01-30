cd55a389edd667d03094e3918b035a4f49f09dd4
Ernest Webb <ewebb@cs.odu.edu>
2021-01-29 23:54:56 -0500

    Init Commit

---
+++ app/src/main/java/com/carecorner/MainActivity.java
@@ -0,0 +1,37 @@
+package com.carecorner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin, btnResources, btnRegister, BtnForgotUsername, BtnForgotPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews() {
+        btnLogin = findViewById(R.id.btnLoginMenu);
+        btnResources = findViewById(R.id.btnResources);
+        btnRegister = findViewById(R.id.btnRegister);
+        BtnForgotUsername = findViewById(R.id.btnForgotUsername);
+        BtnForgotPassword = findViewById(R.id.btnForgotPassword);
+    }
+}
