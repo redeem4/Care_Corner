7f5f36a8f39cac1e054465a9574d1b579e4201cd
Ernest Webb <ewebb@cs.odu.edu>
2021-01-31 03:02:39 -0500

    Create Main Menu Activity and changed a couple file names for consistency
    The files that were changed were MainActivity to WelcomeActivity since it would most likely be confused with the MainMenuActivity. Furthermore, the MainMenuActivity has been created/designed, but its inaccessible atm since I ran out of time for tonight.

---
+++ app/src/main/java/com/carecorner/MainMenuActivity.java
@@ -0,0 +1,14 @@
+package com.carecorner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu_activity);
+    }
+}
