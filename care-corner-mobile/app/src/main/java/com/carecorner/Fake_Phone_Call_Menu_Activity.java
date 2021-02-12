b727fcaaba2da90c7216213519ee2076040e4f4c
ewebb <ewebb@cs.odu.edu>
2021-02-12 03:14:12 -0500

    Add Fake Phone Call Menu and placeholder files for its functionality
    The menu is done yet (variables need to be renamed, added functionality, etc.), but I thought I would commit what has been done so far.

---
+++ care-corner-mobile/app/src/main/java/com/carecorner/Fake_Phone_Call_Menu_Activity.java
@@ -0,0 +1,14 @@
+package com.carecorner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fake_Phone_Call_Menu_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fake_phone_call_menu_activity);
+    }
+}
