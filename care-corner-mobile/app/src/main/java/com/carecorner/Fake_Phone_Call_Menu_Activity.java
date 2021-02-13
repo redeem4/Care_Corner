833626be1323a0cd9164e09c085fac416e84dc5f
ewebb <ewebb@cs.odu.edu>
2021-02-13 03:09:29 -0500

    Update Variables in Fake Phone Call Menu and Start work on GUI for that feature
    I added some images and connected the fake phone call activities so that they are available from the main menu as well.

--- care-corner-mobile/app/src/main/java/com/carecorner/Fake_Phone_Call_Menu_Activity.java
+++ care-corner-mobile/app/src/main/java/com/carecorner/Fake_Phone_Call_Menu_Activity.java
@@ -2,13 +2,39 @@ package com.carecorner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Fake_Phone_Call_Menu_Activity extends AppCompatActivity {
+
+    private Button btnCall, btnRecordings, btnSettings;
+    private EditText nameEntryBox, phoneEntryBox, dateEntryBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fake_phone_call_menu_activity);
+
+        initViews();
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Fake_Phone_Call_Menu_Activity.this, Fake_Phone_Call_Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews() {
+        btnCall = findViewById(R.id.btnCall);
+        btnRecordings = findViewById(R.id.btnRecordings);
+        btnSettings = findViewById(R.id.btnSettings);
+        nameEntryBox = findViewById(R.id.nameEntryBox);
+        phoneEntryBox = findViewById(R.id.phoneEntryBox);
+        dateEntryBox = findViewById(R.id.dateEntryBox);
     }
 }
