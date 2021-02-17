90f73ff93054bea25bcea1cbc1a969c2d47642e4
kgrissom <kgris001@odu.edu>
2021-02-16 23:35:45 -0500

    Journal Editor branch with Journal Editor Activity

---
+++ care-corner-mobile/app/src/main/java/com/carecorner/JournalEditorActivity.java
@@ -0,0 +1,9 @@
+package com.carecorner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JournalEditorActivity extends AppCompatActivity {
+    int whyWontThisPush = 1;
+}
