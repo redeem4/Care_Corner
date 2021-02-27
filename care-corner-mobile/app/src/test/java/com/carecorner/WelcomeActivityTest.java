81a7e583c8e55a596e0af00933071d964e76f24b
ewebb <ewebb@cs.odu.edu>
2021-02-27 02:33:03 -0500

    Add Tests for MainMenuActivity and WelcomeActivity
    The tests for the main menu activity are not complete due to the files not yet existing, so I wrote a TODO comment for someone to fix later once those features start being implemented.

---
+++ care-corner-mobile/app/src/test/java/com/carecorner/WelcomeActivityTest.java
@@ -0,0 +1,89 @@
+package com.carecorner;
+
+import android.content.Intent;
+import android.os.Build;
+import android.widget.Button;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import static org.robolectric.Shadows.shadowOf;
+
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowIntent;
+
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = {Build.VERSION_CODES.O_MR1})
+public class WelcomeActivityTest {
+    private WelcomeActivity activity;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        activity = Robolectric.buildActivity(WelcomeActivity.class)
+                .create()
+                .start()
+                .resume()
+                .get();
+    }
+
+    @Test
+    public void loginButtonClickShouldStartNewActivity() throws Exception
+    {
+        ShadowActivity shadowActivity = shadowOf(activity);
+        Button button = (Button) shadowActivity.getContentView().findViewById(R.id.btnLoginMenu);
+        button.callOnClick();
+        Intent startedIntent = shadowActivity.getNextStartedActivity();
+        ShadowIntent shadowIntent = shadowOf(startedIntent);
+        assertEquals(LoginActivity.class, shadowIntent.getIntentClass());
+    }
+
+    @Test
+    public void resourcesButtonClickShouldStartNewActivity() throws Exception
+    {
+        ShadowActivity shadowActivity = shadowOf(activity);
+        Button button = (Button) shadowActivity.getContentView().findViewById(R.id.btnResources);
+        button.callOnClick();
+        Intent startedIntent = shadowActivity.getNextStartedActivity();
+        ShadowIntent shadowIntent = shadowOf(startedIntent);
+        assertEquals(ResourceActivity.class, shadowIntent.getIntentClass());
+    }
+
+    @Test
+    public void registerButtonClickShouldStartNewActivity() throws Exception
+    {
+        ShadowActivity shadowActivity = shadowOf(activity);
+        Button button = (Button) shadowActivity.getContentView().findViewById(R.id.btnRegister);
+        button.callOnClick();
+        Intent startedIntent = shadowActivity.getNextStartedActivity();
+        ShadowIntent shadowIntent = shadowOf(startedIntent);
+        assertEquals(RegisterActivity.class, shadowIntent.getIntentClass());
+    }
+
+    @Test
+    public void forgotUsernameButtonClickShouldStartNewActivity() throws Exception
+    {
+        ShadowActivity shadowActivity = shadowOf(activity);
+        Button button = (Button) shadowActivity.getContentView().findViewById(R.id.btnForgotUsername);
+        button.callOnClick();
+        Intent startedIntent = shadowActivity.getNextStartedActivity();
+        ShadowIntent shadowIntent = shadowOf(startedIntent);
+        assertEquals(ForgotUsernameActivity.class, shadowIntent.getIntentClass());
+    }
+
+    @Test
+    public void forgotPasswordButtonClickShouldStartNewActivity() throws Exception
+    {
+        ShadowActivity shadowActivity = shadowOf(activity);
+        Button button = (Button) shadowActivity.getContentView().findViewById(R.id.btnForgotPassword);
+        button.callOnClick();
+        Intent startedIntent = shadowActivity.getNextStartedActivity();
+        ShadowIntent shadowIntent = shadowOf(startedIntent);
+        assertEquals(ForgotPasswordActivity.class, shadowIntent.getIntentClass());
+    }
+}
