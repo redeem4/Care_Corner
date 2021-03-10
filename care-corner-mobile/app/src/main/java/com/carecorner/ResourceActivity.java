6448a000c03ac375511e83703d4056d6d08fffba
ewebb <ewebb@cs.odu.edu>
2021-03-10 10:05:51 -0500

    Override Back Button Functionality on the rest of the activities
    This is important to prevent bugs from occurring when users are trying to navigate Care Corner.

--- care-corner-mobile/app/src/main/java/com/carecorner/ResourceActivity.java
+++ care-corner-mobile/app/src/main/java/com/carecorner/ResourceActivity.java
@@ -2,6 +2,7 @@ package com.carecorner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ResourceActivity extends AppCompatActivity {
@@ -10,5 +11,16 @@ public class ResourceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resource_activity);
+    }
+
+    /**
+     * Overrides the Back Button functionality to return to the login screen.
+     */
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        Intent intent = new Intent(ResourceActivity.this, WelcomeActivity.class);
+        startActivity(intent);
     }
 }
