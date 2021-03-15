2c3100a2cee6e60ac569506e862e5b4c58c240cf
ccarpent <Maverick7!>
2021-03-14 19:23:33 -0700

    journal reader activity and xml file finish

--- app/src/main/java/com/carecorner/JournalReader.java
+++ app/src/main/java/com/carecorner/JournalReader.java
@@ -14,24 +14,31 @@ public class JournalReader extends AppCompatActivity {
     Button btnEdit;
     //text reader box
     TextView textBox;
+    TextView title;
     //Convert file passed in from JournalActivity to a string
     String text = "";
+    String journalName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.journal_reader_activity);
         textBox = findViewById(R.id.textBox);
+        title = findViewById(R.id.title);
         btnEdit = findViewById(R.id.btnEdit);
+
 
         try {
             text = getIntent().getStringExtra("text");
+            journalName = getIntent().getStringExtra("journalName");
         }
         catch(NullPointerException e) {
             text = " ";
+            journalName = " ";
         }
 
         textBox.setText(text);
+        title.setText(journalName);
 
         String outText = " ";
 
@@ -43,10 +50,19 @@ public class JournalReader extends AppCompatActivity {
 //                Toast.makeText(JournalReader.this, "Sending to editor", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(JournalReader.this, JournalEditorActivity.class);
                 intent.putExtra("text", text);
-                setResult(RESULT_OK, intent);
+               // setResult(RESULT_OK, intent);
+                startActivity(intent);
                 finish();
             }
         });
     }
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        Intent intent = new Intent(JournalReader.this, JournalRecyclerMain.class);
+        startActivity(intent);
+    }
+
 }
 
