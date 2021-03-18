7173034498a84026c04cff5e822e5f63ec572812
gustin <gprud001@odu.edu>
2021-03-17 22:36:16 -0400

    Create a basic resources api endpoint to demo and set example.

--- care-corner-api/serverless-api/src/main/java/com/carecorner/ResourceHandler.java
+++ care-corner-api/serverless-api/src/main/java/com/carecorner/ResourceHandler.java
@@ -8,28 +8,40 @@ import org.apache.logging.log4j.Logger;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class PanicHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+public class ResourceHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
 	private final Logger logger = LogManager.getLogger(this.getClass());
 
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		logger.debug("Panic Handler received: {}", input);
+		logger.debug("Resources Handler received: {}", input);
 
-		try {
-			// obtain the body from input
-			//JsonNode body = new ObjectMapper().readTree((String)input.get("body"));
-			logger.debug(input.get("name"));
+    Map<String, Object> json;
+
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+      ObjectNode resource = mapper.createObjectNode();
+      resource.put("id", 1);
+      resource.put("name", "National Sexual Assault Hotline");
+      resource.put("phone", "1-800-656-4673");
+      resource.put("category", "hotline");
+
+      json = mapper.convertValue(resource, new TypeReference<Map<String, Object>>(){});
+
+      // print json
+      System.out.println(json);
 
 		} catch (Exception exception) {
-			logger.error("Error in panic: " + exception);
+			logger.error("Error in resource: " + exception);
 
 			// send the error response back
-			Response responseBody = new Response("Error in panic: ", input);
+			Response responseBody = new Response("Error in resource: ", input);
 			return ApiGatewayResponse.builder()
 					.setStatusCode(500)
 					.setObjectBody(responseBody)
@@ -38,7 +50,7 @@ public class PanicHandler implements RequestHandler<Map<String, Object>, ApiGate
 
 		}
 
-		Response responseBody = new Response("Panic received...", input);
+		Response responseBody = new Response("data:", json);
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(responseBody)
