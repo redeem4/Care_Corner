d7a3ac1a7a63338b974ccc4956979d7f820963ce
kgrissom <kgris001@odu.edu>
2021-04-01 19:45:17 -0400

    Mombot basic setup

---
+++ care-corner-mobile/app/src/main/java/com/carecorner/MombotActivity.java
@@ -0,0 +1,26 @@
+package com.carecorner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MombotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.mombot);
+    }
+
+    /**
+     * Overrides the Back Button functionality to return to the Main Menu screen.
+     */
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        Intent intent = new Intent(MombotActivity.this, MainMenuActivity.class);
+        startActivity(intent);
+    }
+}
