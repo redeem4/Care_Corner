b4781e9279ec2ce830480bf308f7f33b4c3b9f2c
gustin <gprud001@odu.edu>
2021-04-03 11:32:26 -0400

    Add AndroidNetworking for easy Restful calls, enable internet access.

--- care-corner-mobile/app/src/main/java/com/carecorner/CareCornerApplication.java
+++ care-corner-mobile/app/src/main/java/com/carecorner/CareCornerApplication.java
@@ -1,11 +1,13 @@
 package com.carecorner;
 
 import android.app.Application;
+import com.androidnetworking.AndroidNetworking;
 
 public class CareCornerApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        AndroidNetworking.initialize(getApplicationContext());
     }
 }
