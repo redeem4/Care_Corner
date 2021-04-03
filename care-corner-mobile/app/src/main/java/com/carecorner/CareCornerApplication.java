7a7420b91b8e211e5701677d7244c4a4d61ecf1d
gustin <gprud001@odu.edu>
2021-04-03 11:02:30 -0400

    Add custom application class for base/common functionality.

---
+++ care-corner-mobile/app/src/main/java/com/carecorner/CareCornerApplication.java
@@ -0,0 +1,11 @@
+package com.carecorner;
+
+import android.app.Application;
+
+public class CareCornerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
