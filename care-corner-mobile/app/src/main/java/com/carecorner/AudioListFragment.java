bef1ccc93ecbc68b0d0567791e4900d2f2af115a
thorrellt <thorrellt@gmail.com>
2021-04-11 01:51:45 -0400

    Recordings List Recycler view properly fetching titles from storage and displaying them

--- care-corner-mobile/app/src/main/java/com/carecorner/AudioListFragment.java
+++ care-corner-mobile/app/src/main/java/com/carecorner/AudioListFragment.java
@@ -2,57 +2,36 @@ package com.carecorner;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AudioListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import java.io.File;
+
+
 public class AudioListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+    private ConstraintLayout playerSheet;
+    private BottomSheetBehavior bottomSheetBehavior;
+
+    private RecyclerView audioList;
+    private File[] allFiles;
+
+    private AudioListAdapter audioListAdapter;
 
     public AudioListFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AudioListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AudioListFragment newInstance(String param1, String param2) {
-        AudioListFragment fragment = new AudioListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -61,4 +40,44 @@ public class AudioListFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_audio_list, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        playerSheet = view.findViewById(R.id.player_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(playerSheet);
+        audioList = view.findViewById(R.id.audio_list_view);
+
+        //setting path to audio files
+        String path = getActivity().getExternalFilesDir("/").getAbsolutePath();
+        File directory = new File(path);
+        allFiles = directory.listFiles();
+
+        audioListAdapter = new AudioListAdapter(allFiles);
+
+        audioList.setHasFixedSize(true);
+        audioList.setLayoutManager(new LinearLayoutManager(getContext()));
+        audioList.setAdapter(audioListAdapter);
+
+        //prevent media player from disappearing at bottom
+        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if(newState == BottomSheetBehavior.STATE_HIDDEN){
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+
+    }
+
+
 }
