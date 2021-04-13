dda89188dcf0fadab169544786585ecff089ce5e
kgrissom <kgris001@odu.edu>
2021-04-13 14:25:56 -0400

    Linked buttons others are not working on to xmls

--- care-corner-mobile/app/src/main/java/com/carecorner/MainMenuActivity.java
+++ care-corner-mobile/app/src/main/java/com/carecorner/MainMenuActivity.java
@@ -25,6 +25,20 @@ public class MainMenuActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btnMomBot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, MombotActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnSafeWalk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, ArmedWalkStart.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void initViews() {
