b650a533ffc4246945e6a3a58a4752df2f822922
kgrissom <kgris001@odu.edu>
2021-04-16 19:23:04 -0400

    Mombot branch set up

--- care-corner-mobile/app/src/main/java/com/carecorner/MombotActivity.java
+++ care-corner-mobile/app/src/main/java/com/carecorner/MombotActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MombotActivity extends AppCompatActivity {
 
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
