7a5c859c52bda817fdf6d6214001485bd3a9480c
thorrellt <thorrellt@gmail.com>
2021-04-17 18:40:03 -0400

    panic mode UI updates.

--- care-corner-mobile/app/src/main/java/com/carecorner/PanicFragment.java
+++ care-corner-mobile/app/src/main/java/com/carecorner/PanicFragment.java
@@ -1,5 +1,6 @@
 package com.carecorner;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,8 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +25,17 @@ public class PanicFragment extends Fragment implements View.OnClickListener {
     private NavController panicNavController;
 
 
+    //panic button and its states
+    private ToggleButton activate_btn;
+    private static final String DEACTIVATED_TEXT = "Activate";
+    private static final String ACTIVATED_TEXT = "Deactivate";
+    private static final boolean  PANIC_ACTIVATED = true;
+    private static final boolean  PANIC_DEACTIVATED = false;
+    private boolean panic_activated = false;
+
+    private TextView panic_status;
+    private static final String PANIC_STATUS_ACTIVATED = "Panic Mode is currently activated";
+    private static final String PANIC_STATUS_DEACTIVATED = "Panic Mode is currently deactivated";
 
 
 
@@ -43,6 +57,21 @@ public class PanicFragment extends Fragment implements View.OnClickListener {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        //set up panic_btn states
+        activate_btn = view.findViewById(R.id.activate_btn);
+        activate_btn.setText(R.string.panic_btn_deactivated);
+        activate_btn.setTextOff(DEACTIVATED_TEXT);
+        activate_btn.setTextOn(ACTIVATED_TEXT);
+
+        panic_status = view.findViewById(R.id.panic_status);
+
+        //set panic to deactivated state
+        activate_btn.setChecked(PANIC_DEACTIVATED);
+        panic_status.setText(PANIC_STATUS_DEACTIVATED);
+
+        //set click listeners
+        activate_btn.setOnClickListener(this);
+
 
 
 
@@ -52,6 +81,21 @@ public class PanicFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.activate_btn:
 
+
+                if(panic_activated){
+                    panic_activated = PANIC_DEACTIVATED;
+                    activate_btn.setChecked(PANIC_DEACTIVATED);
+                    panic_status.setText(PANIC_STATUS_DEACTIVATED);
+                }
+
+                else{
+                    panic_activated = PANIC_ACTIVATED;
+                    activate_btn.setChecked(PANIC_ACTIVATED);
+                    panic_status.setText(PANIC_STATUS_ACTIVATED);
+                }
+        }
     }
 }
