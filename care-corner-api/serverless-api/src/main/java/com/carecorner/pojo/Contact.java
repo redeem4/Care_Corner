47fd78d870e255bd11371001b2b1017583729149
gustin <gprud001@odu.edu>
2021-04-17 19:05:49 -0400

    Add contact and journey POJOs for value object store.

---
+++ care-corner-api/serverless-api/src/main/java/com/carecorner/pojo/Contact.java
@@ -0,0 +1,14 @@
+package com.carecorner.pojo;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@Value(staticConstructor = "of")
+public class Contact {
+  Integer contact_id;
+  Integer user_id;
+  String name;
+  String phone;
+}
