bc4373c46b66e47d6d4834096c9e4f788411d5d3
thorrellt <thorrellt@gmail.com>
2021-04-17 03:10:38 -0400

    Init Commit. panic button framework. set up navigation. updated manifest.

---
+++ care-corner-mobile/app/src/main/java/com/carecorner/PanicActivity.java
@@ -0,0 +1,14 @@
+package com.carecorner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PanicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panic);
+    }
+}
