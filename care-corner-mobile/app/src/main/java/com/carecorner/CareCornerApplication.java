106c23473667e490cb4089db59f90dace3719e94
gustin <gprud001@odu.edu>
2021-04-17 21:15:07 -0400

    Change the api to isolate the code to manage easier.

--- care-corner-mobile/app/src/main/java/com/carecorner/CareCornerApplication.java
+++ care-corner-mobile/app/src/main/java/com/carecorner/CareCornerApplication.java
@@ -10,11 +10,13 @@ import androidx.annotation.Nullable;
 import com.androidnetworking.AndroidNetworking;
 
 public class CareCornerApplication extends Application {
-    String api = "http://10.0.2.2:4566/restapis/72tu10ue4m/local/_user_request_";
+    final String apiCode = "6vdnsy91ut";
+    String api = "http://10.0.2.2:4566/restapis/%s/local/_user_request_";
 
     @Override
     public void onCreate() {
         super.onCreate();
+        this.api = String.format(api, apiCode);
         //AndroidNetworking.initialize(getApplicationContext());
 
     }
