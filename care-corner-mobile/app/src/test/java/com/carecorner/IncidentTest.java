bd9eb30b1592b185b62e28230bb663c02c9d1924
thorrellt <thorrellt@gmail.com>
2021-04-20 02:34:00 -0400

    Created Incident.Java, IncidentTest.java, added hamcrest-2.2.jar to lib for testing, and removed unused RecordAudioTest.java

---
+++ care-corner-mobile/app/src/test/java/com/carecorner/IncidentTest.java
@@ -0,0 +1,27 @@
+package com.carecorner;
+
+import org.junit.After;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IncidentTest {
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void incidentConstructorTest(){
+        Incident incident = new Incident();
+
+        System.out.println("Incident id: " + incident.getId());
+        System.out.println("Recording File Name: " + incident.getRecording_file_name());
+        System.out.println("Journey File Name: " + incident.getJourney_file_name());
+        System.out.println("Start Time: " + incident.getStart_time());
+        System.out.println("Stop Time: " + incident.getStop_time());
+        System.out.println("**After Stop Time Set**");
+        incident.setStop_time();
+        System.out.println("Stop Time: " + incident.getStop_time());
+    }
+}
