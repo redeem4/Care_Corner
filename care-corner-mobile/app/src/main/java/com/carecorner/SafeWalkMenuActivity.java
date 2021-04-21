d84cfe29531ff494ef6a29d1794c52f297baf3c2
gustin <gprud001@odu.edu>
2021-04-20 21:03:37 -0400

    Move journey api to new package, take lat/lon variable for waypoint.

--- care-corner-mobile/app/src/main/java/com/carecorner/SafeWalkMenuActivity.java
+++ care-corner-mobile/app/src/main/java/com/carecorner/SafeWalkMenuActivity.java
@@ -3,22 +3,12 @@ package com.carecorner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.androidnetworking.AndroidNetworking;
-import com.androidnetworking.error.ANError;
-import com.androidnetworking.interfaces.OkHttpResponseListener;
-import com.carecorner.util.JourneyApi;
-
-import org.json.JSONObject;
-
-import okhttp3.Response;
+import com.carecorner.api.JourneyApi;
 
 public class SafeWalkMenuActivity extends AppCompatActivity {
     Button btnStartWalk, btnWalking, btnArrived;
