e51b06986f6b88fa742cfde0d643bd6ac6d80308
thorrellt <thorrellt@gmail.com>
2021-04-21 03:41:46 -0400

    converted SafeWalkMenuActivity to SafeWalkFragment in order to add map to these screens

--- care-corner-mobile/app/src/main/java/com/carecorner/SafeWalkMenuActivity.java
+++ care-corner-mobile/app/src/main/java/com/carecorner/SafeWalkMenuActivity.java
@@ -18,57 +18,9 @@ public class SafeWalkMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.safe_walk_menu_activity);
-        initViews();
 
-        btnStartWalk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startWalk();
-            }
-        });
-
-        btnWalking.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                continueWalk();
-            }
-        });
-
-        btnArrived.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                endWalk();
-            }
-        });
     }
 
-    private void initViews() {
-        btnStartWalk = findViewById(R.id.btnStartWalk);
-        btnArrived = findViewById(R.id.btnArrived);
-        btnWalking = findViewById(R.id.btnWalking);
-        destinationEntryBox = findViewById(R.id.destinationEntryBox);
-        etaEntryBox = findViewById(R.id.etaEntryBox);
-    }
-
-
-    private void startWalk() {
-        String destination = destinationEntryBox.getText().toString();
-        String eta = etaEntryBox.getText().toString();
-
-        CareCornerApplication.getSession().setArmedWalkState(true);
-        JourneyApi.bonVoyage(destination, eta, "80.00", "30.00");
-    }
-
-
-    private void continueWalk() {
-        JourneyApi.wayPoint("80.00", "30.00");
-    }
-
-
-    private void endWalk() {
-        CareCornerApplication.getSession().setArmedWalkState(false);
-        JourneyApi.arrived("80.00", "30.00");
-    }
 
     /**
      * Overrides the Back Button functionality to return to the welcome screen.
